d72326747580b0f067346fea28dab476203d0c2b
akash <akashmollick86@gmail.com>
2022-02-27 00:51:31 +0530

    deleted note

---
+++ JavaScript_Calulator/index.js
@@ -0,0 +1,13 @@
+// function screen(){
+// screen= document.getElementById('screen');
+
+// buttons = document.querySelector('button');
+// for(item of buttons){
+//    item.addEventListener('click', (e)=>{
+//        buttonText=e.target.innerText;
+//        console.log('Button text is :', buttonText);
+       
+//    })
+// }
+// }
+//console.log("Akash")
